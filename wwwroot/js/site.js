76cf2729e3ffa0e7aa4bc78bdf74629ded08a805
Anton Lindström <anton.jumkil@gmail.com>
2024-05-01 20:17:15 +0200

    Fixed close button on modal

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,40 +1,42 @@
-const validateText = (event) => {
-
-    document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Minst 4 tecken"
-    
-}
-
-
-
-//MODAL POPUP --------------------------------------------------------
+//MODAL POPUP ---------------------------------------------------------------------------
+const seeMoreProductInfo = (modalId) => {
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-btn.onclick = function () {
+var modal = document.getElementById(modalId);
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+// Get the <spans> elements that closes the modal
+var spans = document.getElementsByClassName("close");
+
+for (var i = 0; i < spans.length; i++) {
+    spans[i].onclick = function () {
+        console.log("hej");
+        let modals = document.getElementsByClassName("modal");
+        console.log("hej 2");
+        for (const modal of modals) {
+            modal.style.display = "none";
+        }
+    };
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-if (event.target == modal) {
-        modal.style.display = "none";
-}
+    let modals = document.getElementsByClassName("modal");
+
+    for (const modal of modals) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 }
 
+//validating functions ----NOT IN USE---------------------------------------------------------
+const validateText = (event) => {
 
-//validating functions ------------------------------------------------------
+    document.querySelector(`[data-valmsg-for="${event.target.id}"]`).innerHTML = "Minst 4 tecken"
+}
+
 const validateEmail = (event) => {
     const regEx = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
 
