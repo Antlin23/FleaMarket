5d96ef2bdb5a258d3e2bd1ed0230e090a86f3f3a
Anton Lindström <anton.jumkil@gmail.com>
2024-05-13 18:26:44 +0200

    Made so that open btngroups closes if user click anywhere on screen

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -38,6 +38,9 @@ window.onclick = function (event) {
 
 //SHOW AND HIDE BTN GROUPS --
 const showConfirmationGroup = (btnGroup, btnConfirmationGroup, productId) => {
+    //closes all confirmation groups before showing this one
+    hideConfirmationGroup();
+
     if (productId == null) {
         var btnGroup = document.getElementById(btnGroup);
         var btnConfirmationGroup = document.getElementById(btnConfirmationGroup);
@@ -50,18 +53,36 @@ const showConfirmationGroup = (btnGroup, btnConfirmationGroup, productId) => {
     btnConfirmationGroup.classList.remove("d-none");
 }
 
-const hideConfirmationGroup = (btnGroup, btnConfirmationGroup, productId) => {
-    if (productId == null) {
-        var btnGroup = document.getElementById(btnGroup);
-        var btnConfirmationGroup = document.getElementById(btnConfirmationGroup);
+const hideConfirmationGroup = () => {
+    const singleBtnGroups = document.getElementsByClassName("singleBtnGroup");
+    const btnConfirmationGroups = document.getElementsByClassName("btnConfirmationGroup");
+
+    // Convert HTMLCollections to arrays for iteration
+    const singleBtnArray = Array.from(singleBtnGroups);
+    const btnConfirmationArray = Array.from(btnConfirmationGroups);
+
+    // Remove "d-none" class from singleBtnGroups
+    singleBtnArray.forEach((singleBtnGroup) => {
+        singleBtnGroup.classList.remove("d-none");
+    });
+
+    // Add "d-none" class to btnConfirmationGroups
+    btnConfirmationArray.forEach((btnConfirmationGroup) => {
+        btnConfirmationGroup.classList.add("d-none");
+    });
+
+};
+
+document.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+
+    if (!clickedElement.classList.contains("show-congroup-btn")) {
+        hideConfirmationGroup();
     }
-    else {
-        var btnGroup = document.getElementById(btnGroup + productId);
-        var btnConfirmationGroup = document.getElementById(btnConfirmationGroup + productId);
-    }
-    btnGroup.classList.remove("d-none");
-    btnConfirmationGroup.classList.add("d-none");
-}
+});
+
+
+
 
 
 
